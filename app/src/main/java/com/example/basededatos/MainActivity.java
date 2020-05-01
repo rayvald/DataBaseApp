938349cf926cb1d4551541baab6bcb25f19e2197
Osvaldo Murillo <osv.murillo7@gmail.com>
2020-05-01 00:47:24 -0500

    Update records in SQLite

--- app/src/main/java/com/example/basededatos/MainActivity.java
+++ app/src/main/java/com/example/basededatos/MainActivity.java
@@ -45,32 +45,31 @@ public class MainActivity extends AppCompatActivity {
             et_description.setText("");
             et_price.setText("");
 
-            Toast.makeText(this, "Article created successfully", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Article successfully created", Toast.LENGTH_SHORT).show();
 
         } else {
             Toast.makeText(this, "Empty Fields", Toast.LENGTH_SHORT).show();
         }
-
     }
 
     public void search(View view){
         AdminSQliteOpenHelper admin = new AdminSQliteOpenHelper(this, "administration", null, 1);
-        SQLiteDatabase database = admin.getWritableDatabase();
+        SQLiteDatabase dataBase = admin.getWritableDatabase();
 
         String code = et_code.getText().toString();
 
         if (!code.isEmpty()){
-            Cursor row = database.rawQuery
+            Cursor row = dataBase.rawQuery
                     ("select description, price from articles where code =" + code, null);
 
             if (row.moveToFirst()){ //moveToFirst valida que nuestra consulta contenga valores
                 et_description.setText(row.getString(0));
                 et_price.setText(row.getString(1));
 
-                database.close();
+                dataBase.close();
             } else {
                 Toast.makeText(this, "Product does not exist", Toast.LENGTH_SHORT).show();
-                database.close();
+                dataBase.close();
             }
 
         } else {
@@ -80,13 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void delete(View view){
         AdminSQliteOpenHelper admin = new AdminSQliteOpenHelper(this, "administration", null, 1);
-        SQLiteDatabase database = admin.getWritableDatabase();
+        SQLiteDatabase dataBase = admin.getWritableDatabase();
 
         String code = et_code.getText().toString();
 
         if (!code.isEmpty()){
-            int cant = database.delete("articles", "code="+code, null);
-            database.close();
+            int cant = dataBase.delete("articles", "code="+code, null);
+            dataBase.close();
 
             et_code.setText("");
             et_description.setText("");
@@ -99,7 +98,36 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             Toast.makeText(this, "Please Insert a Product Code", Toast.LENGTH_SHORT).show();
-            database.close();
+            dataBase.close();
         }
+    }
+
+    public void update(View view){
+        AdminSQliteOpenHelper admin = new AdminSQliteOpenHelper(this, "administration", null, 1);
+        SQLiteDatabase dataBase = admin.getWritableDatabase();
+
+        String code = et_code.getText().toString();
+        String description = et_description.getText().toString();
+        String price = et_price.getText().toString();
+
+        if(!code.isEmpty() && !description.isEmpty() && !price.isEmpty()){
+            ContentValues reg  = new ContentValues();
+            reg.put("code", code);
+            reg.put("description", description);
+            reg.put("price", price);
+
+            int cant = dataBase.update("articles",reg, "code="+code,null);
+            dataBase.close();
+
+            if (cant == 1){
+                Toast.makeText(this, "Article successfully updated  ", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(this, "The article could not be updated ", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Empty Fields", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
